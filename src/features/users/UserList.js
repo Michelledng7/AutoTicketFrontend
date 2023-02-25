b24cb215d8dcf4e794d7385ddf1f1ad39d71f0df
Michelle <michelledng7@gmail.com>
2023-02-25 23:59:32 +1100

    add table

--- src/features/users/UserList.js
+++ src/features/users/UserList.js
@@ -2,10 +2,33 @@ import { useGetUsersQuery } from './usersApiSlice';
 import { selectAllUsers, selectById, selectIds } from './usersApiSlice';
 
 const UserList = () => {
-	const { data, error, isLoading } = useGetUsersQuery();
+	const { data, error, isLoading, isSuccess, isError } = useGetUsersQuery();
 	console.log(data);
 
-	return <h1>UserList: {data.ids[0]}</h1>;
+	let content;
+	if (isLoading) content = <h1>Loading...</h1>;
+	if (isError) content = <p> Error:{error.data.message} </p>;
+	if (isSuccess) {
+		const { ids } = data;
+		const tableContent = ids?.length
+			? ids.map((userId) => {
+					<User key={userId} userId={userId} />;
+			  })
+			: null;
+		content = (
+			<table>
+				<thead>
+					<tr>
+						<th>UserName</th>
+						<th>Roles</th>
+						<th>Edit</th>
+					</tr>
+				</thead>
+				<tbody>{tableContent}</tbody>
+			</table>
+		);
+	}
+	return content;
 };
 
 export default UserList;
