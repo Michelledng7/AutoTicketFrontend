e4688652017920e4d871b6355d11b35f9050d96f
Michelle <michelledng7@gmail.com>
2023-02-26 01:46:11 +1100

    add user component and css for table

---
+++ src/features/users/User.js
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPen, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { useNavigate } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+import { selectUserById } from './usersApiSlice';
+
+const User = ({ userId }) => {
+	const user = useSelector((state) => selectUserById(state, userId));
+	console.log(user);
+	const navigate = useNavigate();
+	if (user) {
+		const handleEdit = () => navigate(`/dash/users/${userId}`);
+		const userRolesString = user.roles.toString().replaceAll(',', ', ');
+		const cellStatus = user.active ? '' : 'table_cell--inactive';
+		return (
+			<tr className='table_row user'>
+				<td className={`table_cell ${cellStatus}`}>{user.username}</td>
+				<td className={`table__cell ${cellStatus}`}>{userRolesString}</td>
+				<td className={`table__cell ${cellStatus}`}>
+					<button className='icon-button table__button' onClick={handleEdit}>
+						<FontAwesomeIcon icon={faPenToSquare} />
+					</button>
+				</td>
+			</tr>
+		);
+	} else return null;
+};
+
+export default User;
--- src/features/users/UserList.js
+++ src/features/users/UserList.js
@@ -1,5 +1,6 @@
 import { useGetUsersQuery } from './usersApiSlice';
 import { selectAllUsers, selectById, selectIds } from './usersApiSlice';
+import User from './User';
 
 const UserList = () => {
 	const { data, error, isLoading, isSuccess, isError } = useGetUsersQuery();
@@ -10,21 +11,28 @@ const UserList = () => {
 	if (isError) content = <p> Error:{error.data.message} </p>;
 	if (isSuccess) {
 		const { ids } = data;
+		console.log(ids);
 		const tableContent = ids?.length
 			? ids.map((userId) => {
 					<User key={userId} userId={userId} />;
 			  })
 			: null;
 		content = (
-			<table>
-				<thead>
+			<table className='table table--users'>
+				<thead className='table__thead'>
 					<tr>
-						<th>UserName</th>
-						<th>Roles</th>
-						<th>Edit</th>
+						<th scope='col' className='table__th user__username'>
+							UserName
+						</th>
+						<th scope='col' className='table__th user__roles'>
+							Roles
+						</th>
+						<th scope='col' className='table__th user__edit'>
+							Edit
+						</th>
 					</tr>
 				</thead>
-				<tbody>{tableContent}</tbody>
+				<tbody>tc{tableContent}</tbody>
 			</table>
 		);
 	}
