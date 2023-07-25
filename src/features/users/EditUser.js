94a28e714dea4120f42530e69c38ff8c7847fb99
Michelle <michelledng7@gmail.com>
2023-07-25 21:20:32 +1000

    add User Edit form

--- src/features/users/EditUser.js
+++ src/features/users/EditUser.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { selectUserById } from './usersApiSlice';
+import EditUserForm from './EditUserForm';
 
 const EditUser = () => {
-  return (
-    <div>EditUser</div>
-  )
-}
+	const { id } = useParams();
+	const user = useSelector((state) => selectUserById(state, id));
+	const content = user ? <EditUserForm user={user} /> : 'Loading User';
 
-export default EditUser
+	return content;
+};
+
+export default EditUser;
