cf8f1d8f39eae6bef080b3126df49ce02f13df61
Michelle <michelledng7@gmail.com>
2023-09-28 22:08:17 +1000

    change useSelector() to useGetUsersQuery()

--- src/features/Tickets/NewTicket.js
+++ src/features/Tickets/NewTicket.js
@@ -1,14 +1,20 @@
-import { useSelector } from 'react-redux';
-import { selectAllUsers } from '../users/usersApiSlice';
-import NewTicketForm from './NewTicketForm';
+// import { useSelector } from 'react-redux';
+// import { selectAllUsers } from '../users/usersApiSlice';
+import NewTicketForm from './NewTicketForm'
+import { useGetUsersQuery } from '../users/usersApiSlice'
 
 const NewTicket = () => {
-	const users = useSelector(selectAllUsers);
+	//const users = useSelector(selectAllUsers);
+	const { users } = useGetUsersQuery('userList', {
+		selectFromResult: ({ data }) => ({
+			users: data?.ids.map(userId => data?.entities[userId]),
+		}),
+	})
+	console.log(users)
+	if (!users?.length) return <p>Not Currently Available</p>
+	const content = <NewTicketForm users={users} />
 
-	if (!users?.length) return <p>Not Currently Available</p>;
-	const content = <NewTicketForm users={users} />;
+	return content
+}
 
-	return content;
-};
-
-export default NewTicket;
+export default NewTicket
