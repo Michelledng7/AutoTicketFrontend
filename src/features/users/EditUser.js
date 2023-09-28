17bcd73e098c20894e95f89850082d1ab844040a
Michelle <michelledng7@gmail.com>
2023-09-28 22:22:37 +1000

    useGetUsersQuery to get each user

--- src/features/users/EditUser.js
+++ src/features/users/EditUser.js
@@ -1,14 +1,20 @@
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import { selectUserById } from './usersApiSlice';
-import EditUserForm from './EditUserForm';
+import { useParams } from 'react-router-dom'
+// import { useSelector } from 'react-redux';
+// import { selectUserById } from './usersApiSlice';
+import EditUserForm from './EditUserForm'
+import { useGetUsersQuery } from './usersApiSlice'
 
 const EditUser = () => {
-	const { id } = useParams();
-	const user = useSelector((state) => selectUserById(state, id));
-	const content = user ? <EditUserForm user={user} /> : 'Loading User';
+	const { id } = useParams()
+	//const user = useSelector((state) => selectUserById(state, id));
+	const { user } = useGetUsersQuery('userList', {
+		selectFromResult: ({ data }) => ({
+			user: data?.entities[id],
+		}),
+	})
+	const content = user ? <EditUserForm user={user} /> : 'Loading User'
 
-	return content;
-};
+	return content
+}
 
-export default EditUser;
+export default EditUser
